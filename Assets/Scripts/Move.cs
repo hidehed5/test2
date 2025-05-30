7e744c2578925ffbdf20bf393042207a6e515c12
unknown <yuna20091112@gmail.com>
2025-05-30 20:41:20 +0900

    마무리

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Move : MonoBehaviour
+{
+    int player_speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Key)
+        {
+            
+        }
+    }
+}
